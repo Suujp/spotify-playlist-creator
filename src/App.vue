e6df0bc14ef96024ecae073a3cff804db160e542
Suujp <inflames1012@gmail.com>
2022-10-30 18:10:58 +0900

    ヘッダーを追加しました。

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,64 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar app dark color="green darken-1">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>プレイリスト作成アプリ</v-toolbar-title>
     </v-app-bar>
 
+    <v-navigation-drawer app v-model="drawer">
+      <v-container>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">メニュー</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+        
+        <v-list nav dense>
+          <v-list-item v-for="item in items" :key="item" :to="item.link">
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.name }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-container>
+    </v-navigation-drawer>
+
     <v-main>
-      <router-view/>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  data: function() {
+    return {
+      drawer: true,
+      items: [
+        {
+          name: 'ホーム',
+          icon: 'mdi-home',
+          link: '/'
+        },
+        {
+          name: '検索',
+          icon: 'mdi-magnify',
+          link: ''
+        },
+        {
+          name: 'プレイリスト',
+          icon: 'mdi-play',
+          link: ''
+        }
+      ]
+    }
+  }
+}
 </script>
